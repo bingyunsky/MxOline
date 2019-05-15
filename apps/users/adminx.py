9108258e61c83bed45542185421881870551f205
bingyunsky <careerpro@163.com>
2019-05-15 22:50:37 +0800

    更换xadmin，添加验证码

---
+++ apps/users/adminx.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# Author:Vincent
+
+import xadmin
+
+from .models import EmailVerifyRecord
+
+# xadmin中这里是继承object，不再是继承admin
+class EmailVerifyRecordAdmin(object):
+    # 显示列
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索的字段，不要添加时间搜索
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
